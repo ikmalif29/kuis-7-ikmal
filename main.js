a2f752964062f09a3f1700a441b2c1df4c7dcc89
ikmalif29 <ikmalfauzaeni@gmail.com>
2024-07-01 19:11:48 +0700

    kuis

---
+++ main.js
@@ -0,0 +1,268 @@
+let product = [
+    {
+        id: 1,
+        name: "laptop",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 1,
+        name: "laptop",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 1,
+        name: "laptop",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 1,
+        name: "laptop",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 1,
+        name: "laptop",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 1,
+        name: "laptop",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 1,
+        name: "laptop",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 1,
+        name: "laptop",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 1,
+        name: "laptop",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 1,
+        name: "laptop",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 1,
+        name: "laptop",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 1,
+        name: "laptop",
+        gambar: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08227569_10.png",
+        price: "IDR 5.900.000,00",
+        desc: [
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+];
+
+let big = document.querySelector(".big-container")
+big.style.width = "100%"
+big.style.height = "100vh"
+big.style.gap = "40px"
+big.style.justifyContent = "center"
+big.style.alignItems = "center"
+big.style.display = "grid"
+big.style.gridTemplateColumns = "1fr 1fr 1fr 1fr"
+
+product.forEach((p) => {
+    let container = document.createElement("div");
+    container.style.backgroundColor = "white"
+    container.style.border = "2px solid black"
+    container.style.width = "300px"
+    container.style.height = "500px"
+    container.style.marginLeft = "10px"
+    container.style.display = "flex"
+    container.style.cursor = "pointer"
+    container.style.justifyContent = "center"
+    container.style.flexDirection = "column"
+    container.style.textAlign = "center"
+    container.style.borderRadius = "50px"
+    container.style.cursor = "pointer"
+    container.style.boxShadow = "0 0 20px black" 
+
+    let figure = document.createElement("figure")
+    let figcaption = document.createElement("figcaption")
+    let image = document.createElement("img")
+    image.src = p.gambar;
+    image.style.width = "100px"
+    image.style.height = "100px"
+    let h1 = document.createElement("h1")
+    h1.textContent = p.name
+    let h2 = document.createElement("h2")
+    h2.textContent = p.price
+    let buy = document.createElement("button")
+    buy.textContent = "beli sekarang"
+    buy.style.margin = "2px"
+    buy.style.backgroundColor = "blue"
+    let like = document.createElement("button")
+    like.textContent = "like"
+    like.style.margin = "2px"
+    like.style.backgroundColor = "blue"
+    let coment = document.createElement("button")
+    coment.textContent = "coment"
+    coment.style.margin = "2px"
+    coment.style.backgroundColor = "blue"
+    let ul = document.createElement("ul")
+    
+    p.desc.forEach((d) => {
+        let li = document.createElement("li")
+        li.textContent = d
+        ul.appendChild(li)
+    })
+
+    buy.addEventListener("click" , () =>{
+        confirm(`apakah anda akan membeli produk ini ?`)
+    })
+    buy.addEventListener("mouseover" , () =>{
+        buy.style.backgroundColor = "red"
+    })
+    buy.addEventListener("mouseout" , () =>{
+        buy.style.backgroundColor = "blue"
+    })
+    
+    
+    like.addEventListener("click" , () => {
+        alert(`kamu menyukai produk ini`)
+    })
+    like.addEventListener("mouseover" , () =>{
+        like.style.backgroundColor = "red"
+    })
+    like.addEventListener("mouseout" , () =>{
+        like.style.backgroundColor = "blue"
+    })
+    
+    coment.addEventListener("click" , () => {
+        prompt(`apa pendapat anda tentang produk ini ?`)
+    })
+    coment.addEventListener("mouseover" , () =>{
+        coment.style.backgroundColor = "red"
+    })
+    coment.addEventListener("mouseout" , () =>{
+        coment.style.backgroundColor = "blue"
+    })
+
+    figcaption.appendChild(h1)
+    figcaption.appendChild(h2)
+    figcaption.appendChild(ul)
+    figcaption.appendChild(buy)
+    figcaption.appendChild(like)
+    figcaption.appendChild(coment)
+    figure.appendChild(image)
+    figure.appendChild(figcaption)
+    container.appendChild(figure)
+    big.appendChild(container)
+})
